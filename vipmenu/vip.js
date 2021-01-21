5fd549fcbefca073e5bbe6c356fe29a449897a5f
adam1920-code <75556463+adam1920-code@users.noreply.github.com>
2021-01-21 13:11:04 +0700

    Update vip.js

--- vipmenu/vip.js
+++ vipmenu/vip.js
@@ -3,18 +3,18 @@ const vip = (prefix) => {
 	╔═════♬*VIP*♬════
 	╠➥ _*${prefix}hentai*_
 	╠➥ _*${prefix}emojitopng <query>*_
-	╠➥ _*${prefix}text3d <text>*_
+	╠➥ _*${prefix}joox <text>*_
 	╠➥ _*${prefix}resepmakan <keu>*_
 	╠➥ _*${prefix}nsfwanime*
 	╚════♬*VIP*♬══════
 	
-	*[❗] FITUR VIP PRICE: 20K/MONTH*
+	*[❗] FITUR VIP PRICE: 15K/MONTH*
 
 
 	*[❗] FITUR ADMIN PRICE 12K/MOTH*
 	
 	_Minat???_
-	_Chat_:https://wa.me/+6281539336835
+	_Chat_:https://wa.me/+6283153843600
 	
 	_Jangan Lupa Untuk Donasi 🙂_`
 
