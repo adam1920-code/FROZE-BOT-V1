3834ad5c4385179549193c70a0e0566bf60bd16b
adam1920-code <75556463+adam1920-code@users.noreply.github.com>
2021-01-21 16:53:47 +0700

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,102 +1,106 @@
 const help = (prefix) => {
 	return ` 
 ╔══☾❣️ YEONGSILL BOT ❣️☽═
-║❥ *->${prefix}loli*
-║❥ *->${prefix}speed*
-║❥ *->${prefix}getses*
-║❥ *->${prefix}ytmp4 <link- yt>*
-║❥ *->${prefix}ytm3 <link yt>*
-║❥ *->${prefix}toimg <reply sticker>*
-║❥ *->${prefix}sticker*
-║❥ *->${prefix}simih*
-║❥ *->${prefix}tsticker*
-║❥ *->${prefix}nsfwloli*
-║❥ *->${prefix}husbu*
-║❥ *->${prefix}neko*
-║❥ *->${prefix}waifu1*
-║❥ *->${prefix}tebakgambar*
-║❥ *->${prefix}nulis2 <text>*
-║❥ *->${prefix}artinama <text>*
-║❥ *->${prefix}artimimpi <text>*
-║❥ *->${prefix}meme*
-║❥ *->${prefix}memeindo*
-║❥ *->${prefix}gtts*
-║❥ *->${prefix}url12img <url>*
-║❥ *->${prefix}ocr*
-║❥ *->${prefix}firetext <text>*
-║❥ *->${prefix}wait*
-║❥ *->${prefix}pastebin [nama|teks]
-║❥ *->${prefix}phubcomment [nama|url|teks]*
-║❥ *->${prefix}add [+62×××××××××]*
-║❥ *->${prefix}kick @tagmember*
-║❥ *->${prefix}promote @tagmember*
-║❥ *->${prefix}demote @tagmember*
-║❥ *->${prefix}linkgroup*
-║❥ *->${prefix}leave*
-║❥ *->${prefix}tagall*
-║❥ *->${prefix}changlog*
-║❥ *->${prefix}info*
-║❥ *->${prefix}donasi*
-║❥ *->${prefix}tutorial*
-║❥ *->${prefix}infogc*
-║❥ *->${prefix}tiktokstalk @username*
-║❥ *->${prefix}listadmin*
-║❥ *->${prefix}welcome*
-║⚛ *->usege !welcome 1 = on*
-║⚛ *->usege !welcome 0 = off*
-║❥ *->${prefix}blocklist*
-║❥ *->${prefix}kerangmenu*
-║❥ *->${prefix}door [1/0]*
-║❥ *->${prefix}setname*
-║❥ *->${prefix}setdesc*
-║❥ *->${prefix}toxic <text>*
-║❥ *->${prefix}textglud <text>*
-║❥ *->${prefix}wiki <query>*
-║❥ *->${prefix}thunder <text>*
-║❥ *->${prefix}pornhub <text>*
-║❥ *->${prefix}bloodtext <text>*
-║❥ *->${prefix}watertext <text>*
-║❥ *->${prefix}bluetext <text>*
-║❥ *->${prefix}vipmenu*
-║❥ *->${prefix}nulis <teks>*
-║❥ *->${prefix}pubglogo <teks>*
-║❥ *->${prefix}tomp3 <reply>*
-║❥ *->${prefix}joox <album>*
-║❥ *->${prefix}tiktokdown <url>*
-║❥ *->${prefix}stalkig @username*
-║❥ *->${prefix}simi <teks>*
-║❥ *->${prefix}jokerlogo <teks>*
-║❥ *->${prefix}csgologo <teks>*
-║❥ *->${prefix}kpop*
-║❥ *->${prefix}randomhentai <dosa tanggung sendiri!!!>*
-║❥ *->${prefix}bitly <link>*
-║❥ *->${prefix}tahtah < text >*
-║❥ *->${prefix}emasbutton <teks*
-║❥ *->${prefix}silverbutton <teks*
-║❥ *->${prefix}randomnime*
-║❥ *->${prefix}surah*
-║❥ *->${prefix}play <text>*
-║❥ *->${prefix}nsfw [1/0]*
-║❥ *->nsfw 1 = on 0 = off*
-║❥ *->${prefix}waifu*
-║❥ *->${prefix}ramalajodoh [nama|nama2]*
+║🌸 *->${prefix}loli*
+║🌸 *->${prefix}ssweb*
+║🌸 *->${prefix}wiki*
+║🌸 *->${prefix}speed*
+║🌸 *->${prefix}getses*
+║🌸 *->${prefix}ytmp4 <link- yt>*
+║🌸 *->${prefix}ytm3 <link yt>*
+║🌸 *->${prefix}toimg <reply sticker>*
+║🌸 *->${prefix}sticker*
+║🌸 *->${prefix}simih*
+║🌸 *->${prefix}tsticker*
+║🌸 *->${prefix}nsfwloli*
+║🌸 *->${prefix}husbu*
+║🌸 *->${prefix}neko*
+║🌸 *->${prefix}waifu1*
+║🌸 *->${prefix}tebakgambar*
+║🌸 *->${prefix}nulis2 <text>*
+║🌸 *->${prefix}artinama <text>*
+║🌸 *->${prefix}artimimpi <text>*
+║🌸 *->${prefix}meme*
+║🌸 *->${prefix}memeindo*
+║🌸 *->${prefix}gtts*
+║🌸 *->${prefix}url12img <url>*
+║🌸 *->${prefix}ocr*
+║🌸 *->${prefix}firetext <text>*
+║🌸 *->${prefix}wait*
+║🌸 *->${prefix}pastebin [nama|teks]
+║🌸 *->${prefix}phubcomment [nama|url|teks]*
+║🌸 *->${prefix}add [+62×××××××××]*
+║🌸 *->${prefix}kick @tagmember*
+║🌸 *->${prefix}promote @tagmember*
+║🌸 *->${prefix}demote @tagmember*
+║🌸 *->${prefix}linkgroup*
+║🌸 *->${prefix}leave*
+║🌸 *->${prefix}tagall*
+║🌸 *->${prefix}changlog*
+║🌸 *->${prefix}info*
+║🌸 *->${prefix}donasi*
+║🌸 *->${prefix}tutorial*
+║🌸 *->${prefix}infogc*
+║🌸 *->${prefix}tiktokstalk @username*
+║🌸 *->${prefix}listadmin*
+║🌸 *->${prefix}welcome*
+║🌽 *->usege !welcome 1 = on*
+║🌽 *->usege !welcome 0 = off*
+║🌸 *->${prefix}blocklist*
+║🌸 *->${prefix}kerangmenu*
+║🌸 *->${prefix}closegc*
+║🌸 *->${prefix}opengc*
+║🌸 *->${prefix}setname*
+║🌸 *->${prefix}setdesc*
+║🌸 *->${prefix}toxic <text>*
+║🌸 *->${prefix}textglud <text>*
+║🌸 *->${prefix}wiki <query>*
+║🌸 *->${prefix}thunder <text>*
+║🌸 *->${prefix}pornhub <text>*
+║🌸 *->${prefix}bloodtext <text>*
+║🌸 *->${prefix}watertext <text>*
+║🌸 *->${prefix}bluetext <text>*
+║🌸 *->${prefix}vipmenu*
+║🌸 *->${prefix}nulis <teks>*
+║🌸 *->${prefix}pubglogo <teks>*
+║🌸 *->${prefix}tomp3 <reply>*
+║🌸 *->${prefix}joox <album>*
+║🌸 *->${prefix}tiktokdown <url>*
+║🌸 *->${prefix}stalkig @username*
+║🌸 *->${prefix}simi <teks>*
+║🌸 *->${prefix}jokerlogo <teks>*
+║🌸 *->${prefix}csgologo <teks>*
+║🌸 *->${prefix}kpop*
+║🌸 *->${prefix}randomhentai <dosa tanggung sendiri!!!>*
+║🌸 *->${prefix}bitly <link>*
+║🌸 *->${prefix}tahtah < text >*
+║🌸 *->${prefix}emasbutton <teks*
+║🌸 *->${prefix}silverbutton <teks*
+║🌸 *->${prefix}randomnime*
+║🌸 *->${prefix}surah*
+║🌸 *->${prefix}kbbi*
+║🌸 *->${prefix}play <text>*
+║🌸 *->${prefix}nsfw [1/0]*
+║🌸 *->nsfw 1 = on 0 = off*
+║🌸 *->${prefix}waifu*
+║🌸 *->${prefix}ramalajodoh [nama|nama2]*
 ╠═══════════════
-║ *❲OWNER_MENU❳*
-║ *->${prefix}setprefix*
-║ *->${prefix}bc*
-║ *->${prefix}clearall*
-║ *->${prefix}block @tag user*
-║ *->${prefix}unblock @tag user*
+║🌸 *❲OWNER_MENU❳*
+║🌸 *->${prefix}setprefix*
+║🌸 *->${prefix}bc*
+║🌸*->${prefix}clearall*
+║🌸*->${prefix}block @tag user*
+║🌸 *->${prefix}unblock @tag user*
 ╚══☾❣️ FROZE BOT ❣️☽═
 *📢:BOT Tidak Berjalan 24Jam*
 *📢:Bot On Tergantung Owner*
 ======================
 _*Special Thaks To*_
- *ALLAH SWT*
- *KEDUA ORANG TUA*
- *HP SENDIR*
- *DIRI SENDIRI*
- *TEMEN YANG GAK TAU DIRI*
+🌸 *ALLAH SWT*
+🌸 *KEDUA ORANG TUA*
+🌸 *HP SENDIR*
+🌸 *DIRI SENDIRI*
+🌸*TEMEN YANG GAK TAU DIRI*
 =======================
 ┌─*☾CHANGLOG☽*─┐
 *➥New Ui*
@@ -108,7 +112,7 @@ _*Special Thaks To*_
 
 *Beri Jeda 5 Detik Command!!!*
 
-*[MADE BY 🥶ADAM OEY19🥶]*`
+*[MADE BY 🍀ADAM OEY19🍀]*`
 }
 
 exports.help = help
